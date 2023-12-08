cb5f67ad0dce0cdedb1566e38237f476d2c3f0c8
mohsen <mohsenseyedi1382@gmail.com>
2023-12-08 10:58:23 +0330

    add lastCourses to components

--- src/pages/Index/Index.jsx
+++ src/pages/Index/Index.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import Header from '../../Components/Header/Header'
 
 import './Index.css'
+import LastCourses from '../../Components/LastCourses/LastCourses'
 
 export default function Index() {
   return (
-    <Header />
+    <>
+       <Header />
+       <LastCourses/>
+    </>
   )
 }
