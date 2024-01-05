b8fc252fc04855680c524aad6e13729daeec4629
mohsen <mohsenseyedi1382@gmail.com>
2024-01-05 12:54:37 +0330

    add breadcrumb component to project

--- src/pages/CourseInfo/CourseInfo.jsx
+++ src/pages/CourseInfo/CourseInfo.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
 import './CourseInfo.css'
+import Topbar from '../../Components/Topbar/Topbar'
+import Navbar from '../../Components/Navbar/Navbar'
+import Breadcrumb from '../../Components/Breadcrumb/Breadcrumb'
 
 export default function CourseInfo() {
   return (
-    <div>CourseInfo</div>
+    <>
+    <Topbar/>
+    <Navbar/>
+    <Breadcrumb links={
+      [
+        {id:1 , title:"خانه" , to:""},
+        {id:2 , title:"برنامه نویسی فرانت اند" , to:"category-info/frontend"},
+        {id:3 , title:"دوره متخصص جاوااسکریپت" , to:"course-info/js"},
+      ]
+    }/>
+    </>
   )
 }
