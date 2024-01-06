2adb73aa16a1cb58739273643270f3852d5c4270
mohsen <mohsenseyedi1382@gmail.com>
2024-01-06 22:09:32 +0330

    update course-info.jsx adn css file

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-// import "./index.css";
+
 import './styles/reset.css'
 import './styles/fonts.css'
 import './styles/variables.css'
 import './styles/helpers.css'
 import './styles/defaults.css'
 import "bootstrap/dist/css/bootstrap.min.css";
-
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
