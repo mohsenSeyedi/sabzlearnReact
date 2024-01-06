0be71fe052f0f43e3a77a11bf7cda49144003f9b
mohsen <mohsenseyedi1382@gmail.com>
2024-01-06 21:35:14 +0330

    import bootstrap to main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,9 @@ import './styles/fonts.css'
 import './styles/variables.css'
 import './styles/helpers.css'
 import './styles/defaults.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
